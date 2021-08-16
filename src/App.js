5acd17b7560fd58865bb9a00a49d7ee063bb9bee
hellosudhii <sudhii@seekmy.in>
2021-08-16 21:16:42 +0530

    👌

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+// @ts-check
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative grid h-screen w-screen place-items-center">
+      
+        <div className="absolute">
+          <span className="animate-ping">🖤</span>
+          <span className="animate-pulse">🧡</span>
+        </div>
+      
     </div>
   );
 }
